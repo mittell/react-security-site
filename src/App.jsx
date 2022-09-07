b97964b1ebf879d5cc3d177a578c72847acc0308
Chris Mittell <mittell@hotmail.co.uk>
2022-09-07 18:04:56 +0900

    feat: added Navbar component and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Hero from './components/Hero/Hero';
+import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
 	return (
 		<>
+			<Navbar />
 			<Hero />
 		</>
 	);
