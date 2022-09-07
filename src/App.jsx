3de0f2102c8eb7640d9bd152d25b50473e32ed1a
Chris Mittell <mittell@hotmail.co.uk>
2022-09-07 18:21:43 +0900

    feat: added Cloud component and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Cloud from './components/Cloud/Cloud';
 import Data from './components/Data/Data';
 import Hero from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar';
@@ -9,6 +10,7 @@ const App = () => {
 			<Navbar />
 			<Hero />
 			<Data />
+			<Cloud />
 		</>
 	);
 };
---
+++ src/components/Cloud/Cloud.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './CloudStyles.css';
+
+const Cloud = () => {
+	return (
+		<div className='cloud'>
+			<div className='container'>
+				<div className='content'>
+					<h2>
+						<span>Cloud</span> Security
+					</h2>
+					<p>
+						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi
+						voluptate nihil velit voluptas placeat praesentium nesciunt dolores
+						sunt voluptates, provident ut eligendi distinctio iste, sapiente
+						laboriosam nostrum obcaecati, inventore voluptatum.
+					</p>
+					<div>
+						<button>Sign Up</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Cloud;
