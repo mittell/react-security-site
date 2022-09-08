1bb2314e501aeb9c79ee07c2fdcfb123bf1f42ff
Chris Mittell <mittell@hotmail.co.uk>
2022-09-08 09:30:35 +0900

    feat: added Contact component and styling

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Navbar from '../components/Navbar/Navbar';
-// import Contact from '../components/Contact/Contact';
+import Contact from '../components/Contact/Contact';
 import Footer from '../components/Footer/Footer';
 
 const ContactPage = () => {
 	return (
 		<>
 			<Navbar />
-			{/* <Contact /> */}
+			<Contact />
 			<Footer />
 		</>
 	);
