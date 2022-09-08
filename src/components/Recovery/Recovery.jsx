aef48abc413832e1711e160a3a24e23b517fd9cb
Chris Mittell <mittell@hotmail.co.uk>
2022-09-08 09:06:11 +0900

    feat: added Recovery component and styles

---
+++ src/components/Recovery/Recovery.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './RecoveryStyles.css';
+
+const Recovery = () => {
+	return (
+		<div className='recovery'>
+			<div className='container'>
+				<div className='content'>
+					<h2>
+						<span>Data</span> Recovery
+					</h2>
+					<p>
+						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas
+						amet quasi in optio sequi recusandae sapiente distinctio vel, a
+						natus esse suscipit? Mollitia, aliquid qui nihil repellendus minus
+						commodi necessitatibus?
+					</p>
+					<div>
+						<button>Learn More</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Recovery;
